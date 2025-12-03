c58e9222bde0cf29d2c0951d5f384620ac506be1
dilanithathsarani <dilanithathsarani2003@gmail.com>
2025-12-03 21:29:38 +0530

    google login

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,104 +2,115 @@ import Order from "../models/order.js";
 import Product from "../models/product.js";
 
 export async function createOrder(req, res) {
-   if(req.user == null) {
-        return res.status(401).json({
-            message: "Unauthorized access"
-        });
-        return;
-    }
+	if (req.user == null) {
+		res.status(401).json({
+			message: "Unauthorized",
+		});
+		return;
+	}
 
-    const body= req.body;
+	const body = req.body;
+	const orderData = {
+		orderId: "",
+		email: req.user.email,
+		name: body.name,
+		address: body.address,
+		phoneNumber: body.phoneNumber,
+		billItems: [],
+		total: 0,
+	};
+	Order.find()
+		.sort({
+			date: -1,
+		})
+		.limit(1)
+		.then(async (lastBills) => {
+			if (lastBills.length == 0) {
+				orderData.orderId = "ORD0001";
+			} else {
+				const lastBill = lastBills[0];
+				const lastOrderId = lastBill.orderId; //"ORD0061"
+				const lastOrderNumber = lastOrderId.replace("ORD", ""); //"0061"
+				const lastOrderNumberInt = parseInt(lastOrderNumber); //61
+				const newOrderNumberInt = lastOrderNumberInt + 1; //62
+				const newOrderNumberStr = newOrderNumberInt.toString().padStart(4, "0"); // "0062"
+				orderData.orderId = "ORD" + newOrderNumberStr;
+			}
 
-    const orderData = {
-        orderId: "",
-        email:req.user.email,
-        name: body.name,
-        address: body.address,
-        phoneNumber: body.phoneNumber,
-        billItems:[],
-        total:0
-    };
+			for (let i = 0; i < body.billItems.length; i++) {
+				const product = await Product.findOne({
+					productId: body.billItems[i].productId,
+				});
+				if (product == null) {
+					res.status(404).json({
+						message:
+							"Product with product id " +
+							body.billItems[i].productId +
+							" not found",
+					});
+					return;
+				}
+				
 
-    const lastBills =Order.find().sort({ 
-        date: -1 
-    }).limit(1).then(async(lastBills) => {
-        if(lastBills.length==0) {
-        orderData.orderId = "ORD-0001";
-    }else {
-        const lastBill =lastBills[0];
-        const lastOrderId = lastBill.orderId;
-        const lastOrderNumber = lastOrderId.replace("ORD-", "");
-        const lastOrderNumberInt = parseInt(lastOrderNumber);
-        const newOrderNumberInt = lastOrderNumberInt + 1;
-        const newOrderNumberStr = newOrderNumberInt.toString().padStart(4, "0");
-        orderData.orderId = "ORD-" +newOrderNumberStr;
-    }
+				orderData.billItems[i] = {
+                    productId : product.productId,
+                    productName : product.name,
+                    image : product.images[0],
+                    quantity : body.billItems[i].quantity,
+                    price : product.price
+                };
+                orderData.total = orderData.total + product.price * body.billItems[i].quantity
+			}
 
-    for(let i=0; i<body.billItems.length; i++) {
-       const product = await Product.findOne({
-           productId: body.billItems[i].productId,
-         });
-         if(product == null) {
-            return res.status(400).json({
-                message: "Product with product id " + body.billItems[i].productId + " not found"
-            });
-            return;
-        }
+			const order = new Order(orderData);
 
-        orderData.billItems[i] = {
-            productId: product.productId,
-            name: product.name,
-            image: product.images[0],
-            price: product.price,
-            quantity: body.billItems[i].quantity
-        };
-
-        orderData.total = orderData.total + product.price* body.billItems[i].quantity;
-    }
-
-    const order = new Order(orderData);
-
-    order.save().then(() => {
-        res.json({
-            message: "Order saved successfully",
-        });
-    }).catch((err) => {
-        console.error(err);
-        res.status(500).json({
-            message: "Order not saved"
-        });
-    });
-
-    });
+			order
+				.save()
+				.then(() => {
+					res.json({
+						message: "Order saved successfully",
+					});
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(500).json({
+						message: "Order not saved",
+					});
+				});
+		});
 }
 
 export function getOrders(req, res) {
-    if(req.user == null) {
-        return res.status(401).json({
-            message: "Unauthorized access"
-        });
-    }
-    
-    if(req.user.role == "admin") {
-        Order.find().then((orders) => {
-            res.json(orders);
-        }).catch((err) => {
-            console.error(err);
-            res.status(500).json({
-                message: "Order not found"
-            });
-        });
-    }else{
-        Order.find({ email: req.user.email }).then((orders) => {
-            res.json(orders);
-        }).catch((err) => {
-            console.error(err);
-            res.status(500).json({
-                message: "Order not found"
-            });
-        });
-    }
+	if (req.user == null) {
+		res.status(401).json({
+			message: "Unauthorized",
+		});
+		return;
+	}
+
+	if (req.user.role == "admin") {
+		Order.find()
+			.then((orders) => {
+				res.json(orders);
+			})
+			.catch((err) => {
+				res.status(500).json({
+					message: "Orders not found",
+				});
+			});
+	} else {
+		Order.find({
+			email: req.user.email,
+		})
+			.then((orders) => {
+				res.json(orders);
+			})
+			.catch((err) => {
+				res.status(500).json({
+					message: "Orders not found",
+				});
+			});
+	}
 }
 
 export async function updateOrder(req,res){
@@ -130,3 +141,5 @@ export async function updateOrder(req,res){
 		})
 	}
 }
+
+//https://www.googleapis.com/oauth2/v1/userinfo?
